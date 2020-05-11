2ea800041e9299a92d548c61f906424cf3c687a0
hlp-pls <53251969+hlp-pls@users.noreply.github.com>
2020-05-11 14:46:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-
+//"video"라는 아이디를 가진 html element를 불러옵니다.
 const video = document.getElementById('video');
 
+//Promise .all()에서 사용할 모델을 지정하고, .then()에서 모델이 로딩되면 startVideo 함수를 실행. 
 Promise.all([
+	//얼굴을 감지하기 위해 필요한 저용량 모델 로드 
 	faceapi.nets.tinyFaceDetector.loadFromUri('models/'),
+	//얼굴 랜드마크를 감지하기 위한 모델 로드
 	faceapi.nets.faceLandmark68Net.loadFromUri('models/'),
-	//faceapi.nets.faceRecognition.loadFromUri('/models'),
-	//faceapi.nets.faceExpressionNet.loadFromUri('/models')
 ]).then(startVideo);
 
+//video에 웹캠 스트림을 넣어주는 기능을 수행한다.
 function startVideo(){
 	navigator.getUserMedia(
 		{ video : {} },
@@ -16,36 +18,37 @@ function startVideo(){
 	);
 }
 
-var landmark_points;
-var offset;
+var landmark_points; //모델이 결과로 내놓은 좌표점들을 저장할 변수
 
 video.addEventListener('play', () => {
+	//이후에 모델이 내놓는 점의 좌표들을 현재 비디오 화면 크기에 맞춰주기 위해 비디오 화면 크기를 얻는다.
 	const displaySize = { width: video.width, height: video.height};
 	//setInterval은 일정 시간을 간격으로 실행된다.
 	setInterval(async () => {
+		//모델의 예측 결과 추출 -> 현재 비디오 크기에 맞게 결과 조절 -> 예측결과가 있다면 landmark_points에 좌표값 저장.
 		const detections = await faceapi.detectAllFaces(
 			video,
 			new faceapi.TinyFaceDetectorOptions()
 		).withFaceLandmarks()
-		//console.log(detections)
-		//console.log(detections[0].landmarks)
 		const resizedDetections = faceapi.resizeResults(detections,displaySize);
 		if(resizedDetections&&resizedDetections[0]){
 			landmark_points = resizedDetections[0].landmarks._positions;
-			offset = resizedDetections[0].landmarks._shift;
-			//console.log(resizedDetections[0].landmarks._shift);
 		}
 	}, 50)
 });
 
 
 function setup(){
+	//캔버스를 html 파일에서 지정한 비디오의 크기와 같게 생성
 	createCanvas(720,540);
+	//그리는 기하도형의 채움제거
 	noFill();
+	//획의 두께 2px
 	strokeWeight(2);
 }
 
 function draw(){
+	//이전 화면 지우기
 	clear();
 	if(landmark_points){
 		beginShape();
@@ -56,8 +59,6 @@ function draw(){
 			vertex(x,y);
 		}
 		endShape();
-
-		ellipse(offset._x,offset._y,20,20);
 	}
 
 	
