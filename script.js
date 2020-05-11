6bd64ec102994cbea712fba0d7c74a0ee3c1ae65
hlp-pls <53251969+hlp-pls@users.noreply.github.com>
2020-05-11 14:08:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 const video = document.getElementById('video');
 
 Promise.all([
-	faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-	faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+	faceapi.nets.tinyFaceDetector.loadFromUri('models/'),
+	faceapi.nets.faceLandmark68Net.loadFromUri('models/'),
 	//faceapi.nets.faceRecognition.loadFromUri('/models'),
 	//faceapi.nets.faceExpressionNet.loadFromUri('/models')
 ]).then(startVideo);
